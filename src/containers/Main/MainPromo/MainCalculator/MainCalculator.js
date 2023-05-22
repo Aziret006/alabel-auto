2dac2e10143952217aa5a3cf4327acd7c9cd1d08
aman <amanmaksat0@gmail.com>
2023-05-22 19:22:30 +0600

    updated  main calculator

--- src/containers/Main/MainPromo/MainCalculator/MainCalculator.js
+++ src/containers/Main/MainPromo/MainCalculator/MainCalculator.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Box, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material'
 import { makeStyles } from 'tss-react/mui'
+import { Link } from 'react-router-dom'
+import axiosApi from '../../../../axiosApi'
 
 const useStyles = makeStyles()(theme => ({
   mainInput: {
@@ -19,6 +21,7 @@ const useStyles = makeStyles()(theme => ({
       borderRadius: '3px',
       [theme.breakpoints.down('lx')]: {
         background: 'transparent',
+        color: '#231F1E',
       },
     },
   },
@@ -47,16 +50,100 @@ const useStyles = makeStyles()(theme => ({
 
 const MainCalculator = () => {
   const { classes } = useStyles()
-  const [state, setState] = useState({
+  const [calculator, setCalculator] = useState({
+    auctions: [],
+    locations: [],
+    body: [],
+    ports: [],
+    countries: [],
+    fromPorts: [],
+    total: 0,
+    service_auto_canada: 0,
+  })
+  const [calculateData, setCalculateData] = useState({
     auction: '',
     city: '',
-    fuel: '',
+    port_canada: '',
+    port_destination: '',
+    country_destination: '',
+    body: '',
+    price: 1000,
   })
+
+  useEffect(() => {
+    const getCalculatorData = async () => {
+      try {
+        const auctions = await axiosApi('/car/auction_list/')
+        const body = await axiosApi('/car/body_for_calculator/')
+        const countries = await axiosApi('/car/country_destination/')
+        const ports = await axiosApi('/car/port_destination/')
+
+        setCalculator(prev => ({
+          ...prev,
+          auctions: auctions?.data,
+          body: body?.data,
+          countries: countries?.data,
+          ports: ports?.data,
+        }))
+      } catch {}
+    }
+
+    getCalculatorData().catch(e => console.log(e))
+  }, [])
+
+  useEffect(() => {
+    const calculate = async dataCalculator => {
+      try {
+        const { data } = await axiosApi.post('/car/calculator/', dataCalculator)
+        setCalculator(prev => ({
+          ...prev,
+          total: data.total,
+          service_auto_canada: data.service_auto_canada,
+        }))
+      } catch {}
+    }
+
+    if (
+      calculateData.body &&
+      calculateData.port_canada &&
+      calculateData.port_destination &&
+      calculateData.country_destination &&
+      calculateData.price
+    ) {
+      calculate(calculateData).catch(err => console.log(err))
+    }
+  }, [calculateData])
 
   const changeHandler = e => {
     const { name, value } = e.target
 
-    setState(prev => ({ ...prev, [name]: value }))
+    setCalculateData(prev => ({ ...prev, [name]: value }))
+  }
+
+  const getLocations = async id => {
+    try {
+      const { data } = await axiosApi(`/car/city_list/?auction=${id}`)
+
+      if (data) {
+        setCalculator(prev => ({
+          ...prev,
+          locations: data,
+        }))
+      }
+    } catch {}
+  }
+
+  const getFromPorts = async (auction, zipCode, location) => {
+    try {
+      const { data } = await axiosApi(
+        `/car/port_delivery_list/?auction=${auction}&zip_code=${zipCode}&location=${location}&body=${calculateData.body}`,
+      )
+
+      setCalculator(prev => ({
+        ...prev,
+        fromPorts: data,
+      }))
+    } catch {}
   }
 
   return (
@@ -66,7 +153,7 @@ const MainCalculator = () => {
       border="1px solid #323131"
       borderRadius="15px"
       sx={{
-        marginTop: { lx: '130px', xl: '110px' },
+        marginTop: { lx: '110px', xl: '100px' },
         maxWidth: { lx: '380px', xl: '490px' },
         color: { xs: '#231F1E', lx: '#fff' },
       }}
@@ -75,9 +162,8 @@ const MainCalculator = () => {
         variant="h3"
         fontWeight="500"
         textAlign="center"
-        marginY="24px"
         textTransform="uppercase"
-        sx={{ fontSize: { xs: '24px', lg: '32px', xl: '40px' }, margin: { xs: '20px 0 10px', xl: '24px 0 16px' } }}
+        sx={{ fontSize: { xs: '24px', lg: '32px', xl: '40px' }, margin: { xs: '16px 0 8px', xl: '18px 0 14px' } }}
       >
         <Typography variant="span" color="#F47721">
           ca
@@ -91,7 +177,7 @@ const MainCalculator = () => {
         padding="0 30px 20px"
         sx={{ width: { xs: '280px', lg: '360px', xl: '480px' } }}
       >
-        <Grid item xs={12} sx={{ marginY: { xs: '14px', xl: '20px' } }}>
+        <Grid item xs={6} pr="18px" sx={{ marginY: { xs: '14px', xl: '20px' } }}>
           <FormControl fullWidth color="orange">
             <InputLabel
               id="select-label"
@@ -104,79 +190,204 @@ const MainCalculator = () => {
               className={classes.mainSelect}
               labelId="select-label"
               id="simple-select"
-              value={state.auction}
+              value={calculateData.auction}
               name="auction"
               label="Choose an auction"
               onChange={changeHandler}
             >
-              <MenuItem value="impact">Impact</MenuItem>
-              <MenuItem value="twenty">Twenty</MenuItem>
-              <MenuItem value="third">Thirty</MenuItem>
+              {calculator.auctions?.length !== 0 ? (
+                calculator.auctions.map(auction => (
+                  <MenuItem key={`auction${auction.id}`} value={auction.id} onClick={() => getLocations(auction.id)}>
+                    {auction.title}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem value="">---</MenuItem>
+              )}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6} sx={{ marginY: { xs: '14px', xl: '20px' } }}>
+          <FormControl fullWidth color="orange">
+            <InputLabel id="body-label" sx={{ fontSize: '16px', color: { xs: '#231F1E', lx: '#fff' } }} color="orange">
+              Body
+            </InputLabel>
+            <Select
+              className={classes.mainSelect}
+              labelId="body-label"
+              id="body"
+              value={calculateData.body}
+              name="body"
+              label="Body"
+              onChange={changeHandler}
+            >
+              {calculator.body?.length !== 0 ? (
+                calculator.body.map(body => (
+                  <MenuItem key={`body${body.id}`} value={body.id}>
+                    {body.title}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem value="">---</MenuItem>
+              )}
             </Select>
           </FormControl>
         </Grid>
         <Grid item xs={12} sx={{ marginY: { xs: '14px', xl: '20px' } }}>
           <FormControl fullWidth color="orange">
             <InputLabel
-              id="select-label2"
+              id="location-label"
               sx={{ fontSize: '16px', color: { xs: '#231F1E', lx: '#fff' } }}
               color="orange"
             >
-              Cities
+              Location
             </InputLabel>
             <Select
               className={classes.mainSelect}
-              labelId="select-label2"
-              id="simple-select2"
-              value={state.city}
+              labelId="location-label"
+              id="location"
+              value={calculateData.city}
               name="city"
-              label="Cities"
+              label="Location"
               onChange={changeHandler}
             >
-              <MenuItem value="ab">Albany</MenuItem>
-              <MenuItem value="cn">Canada</MenuItem>
-              <MenuItem value="kg">Kyrgyzstan</MenuItem>
+              {calculator.locations?.length !== 0 ? (
+                calculator.locations.map(location => (
+                  <MenuItem
+                    key={`location${location.id}`}
+                    value={location.id}
+                    onClick={() => getFromPorts(location.auction, location.zip_code, location.location)}
+                  >
+                    {location.location}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem value="">---</MenuItem>
+              )}
             </Select>
           </FormControl>
         </Grid>
-        <Grid item xs={12} sx={{ marginY: { xs: '14px', xl: '20px' } }}>
+        <Grid item xs={6} pr="18px" sx={{ marginY: { xs: '14px', xl: '20px' } }}>
           <TextField
             className={classes.mainInput}
             label="Car price in Canada/USA"
             type="number"
             fullWidth
             color="orange"
+            name="price"
+            value={calculateData.price}
+            onChange={changeHandler}
           />
         </Grid>
-        <Grid item xs={6} pr="7px" sx={{ marginY: { xs: '14px', xl: '20px' } }}>
-          <TextField className={classes.mainInput} label="Year" type="number" fullWidth color="orange" />
-        </Grid>
-        <Grid item xs={6} pl="7px" sx={{ marginY: { xs: '14px', xl: '20px' } }}>
-          <TextField className={classes.mainInput} label="Engine capacity" type="number" fullWidth color="orange" />
-        </Grid>
-        <Grid item xs={12} sx={{ marginY: { xs: '14px', xl: '20px' } }}>
+        <Grid item xs={6} sx={{ marginY: { xs: '14px', xl: '20px' } }}>
           <FormControl fullWidth color="orange">
             <InputLabel
-              id="select-label3"
+              id="Shippingfromtheport-label"
               sx={{ fontSize: '16px', color: { xs: '#231F1E', lx: '#fff' } }}
               color="orange"
             >
-              Fuel type
+              Shipping from the port
             </InputLabel>
             <Select
               className={classes.mainSelect}
-              labelId="select-label3"
-              id="simple-select3"
-              value={state.fuel}
-              name="fuel"
-              label="Fuel type"
+              labelId="Shippingfromtheport-label"
+              id="Shippingfromtheport"
+              value={calculateData.port_canada}
+              name="port_canada"
+              label="Shipping from the port"
               onChange={changeHandler}
             >
-              <MenuItem value="diesel">Diesel</MenuItem>
-              <MenuItem value="benz">benz</MenuItem>
+              {calculator.fromPorts?.length !== 0 ? (
+                calculator.fromPorts.map(port => (
+                  <MenuItem key={`fromPort${port.id}`} value={port.id}>
+                    {port.title}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem value="">---</MenuItem>
+              )}
             </Select>
           </FormControl>
         </Grid>
+        <Grid item xs={6} pr="18px" sx={{ marginY: { xs: '14px', xl: '20px' } }}>
+          <FormControl fullWidth color="orange">
+            <InputLabel
+              id="Destination-label"
+              sx={{ fontSize: '16px', color: { xs: '#231F1E', lx: '#fff' } }}
+              color="orange"
+            >
+              Destination country
+            </InputLabel>
+            <Select
+              className={classes.mainSelect}
+              labelId="Destination-label"
+              id="Destination"
+              value={calculateData.country_destination}
+              name="country_destination"
+              label="Destination country"
+              onChange={changeHandler}
+            >
+              {calculator.countries?.length !== 0 ? (
+                calculator.countries.map(country => (
+                  <MenuItem key={`country${country.id}`} value={country.id}>
+                    {country.title}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem value="">---</MenuItem>
+              )}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6} sx={{ marginY: { xs: '14px', xl: '20px' } }}>
+          <FormControl fullWidth color="orange">
+            <InputLabel id="Port-label" sx={{ fontSize: '16px', color: { xs: '#231F1E', lx: '#fff' } }} color="orange">
+              Port of destination
+            </InputLabel>
+            <Select
+              className={classes.mainSelect}
+              labelId="Port-label"
+              id="Port"
+              value={calculateData.port_destination}
+              name="port_destination"
+              label="Port of destination"
+              onChange={changeHandler}
+            >
+              {calculator.ports?.length !== 0 ? (
+                calculator.ports.map(port => (
+                  <MenuItem key={`port${port.id}`} value={port.id}>
+                    {port.title}
+                  </MenuItem>
+                ))
+              ) : (
+                <MenuItem value="">---</MenuItem>
+              )}
+            </Select>
+          </FormControl>
+        </Grid>
+        {calculator.service_auto_canada ? (
+          <Grid
+            item
+            xs={12}
+            fontWeight="500"
+            padding="10px 15px"
+            sx={{
+              background: { lx: '#3C3C3C' },
+              borderRadius: '3px',
+              margin: { xs: '14px 0 6px', xl: '20px 0 10px' },
+              border: { xs: '1px solid #CBCBCB', lx: 'none' },
+            }}
+          >
+            <Box display="flex" justifyContent="space-between" alignItems="center">
+              <Typography textTransform="uppercase" sx={{ fontSize: { xs: '14px', lx: '15px', xl: '16px' } }}>
+                service
+              </Typography>
+              <Typography color="#F47721" sx={{ fontSize: { xs: '15px', lx: '18px', xl: '24px' } }}>
+                $ {calculator.service_auto_canada}
+              </Typography>
+            </Box>
+          </Grid>
+        ) : null}
         <Grid
           item
           xs={12}
@@ -194,7 +405,7 @@ const MainCalculator = () => {
               total amount
             </Typography>
             <Typography color="#F47721" sx={{ fontSize: { xs: '15px', lx: '18px', xl: '24px' } }}>
-              $ 18 888
+              $ {calculator.total}
             </Typography>
           </Box>
         </Grid>
@@ -210,9 +421,14 @@ const MainCalculator = () => {
               Deposit
             </Typography>
             <Typography color="#F47721" sx={{ fontSize: { xs: '15px', lx: '18px', xl: '24px' } }}>
-              $ 1 888
+              $ {Math.floor(calculator.total * 0.1)}
             </Typography>
           </Box>
+        </Grid>
+        <Grid item xs={12} mt="20px">
+          <Link to="/auction" className="button calculator_btn">
+            Read more
+          </Link>
         </Grid>
       </Grid>
     </Box>
